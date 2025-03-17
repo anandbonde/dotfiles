ddbd2dafdc6b07ffcf8a20bb9f4648699a2d4158
Anand Bonde <anand.bonde@gmail.com>
2025-03-16 19:59:14 -0700

    update nvim config to remove pyright

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -699,7 +699,6 @@ require("lazy").setup({
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"clangd",
-					"pyright",
 					"rust_analyzer",
 				},
 				automatic_installation = true,
