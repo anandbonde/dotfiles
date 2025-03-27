610c8d2a2c7ab496acb9c9a91c63fe68928c2dc0
Anand Bonde <anand.bonde@gmail.com>
2025-03-27 19:29:54 -0400

    update macos tools and wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,10 +9,23 @@ wezterm.on("update-right-status", function(window, _pane)
 end)
 
 return {
-	color_scheme = "tokyonight_storm",
+	-- color_scheme = "tokyonight_storm",
+	-- color_scheme = "OneDark (base16)",
+	-- color_scheme = "OneHalfDark",
+	-- color_scheme = "3024 (base16)",
+	color_scheme = "Catppuccin Frappe",
 	font_size = 16,
 	hide_tab_bar_if_only_one_tab = true,
 	line_height = 1.2,
 	-- macos_window_background_blur = 10,
-	-- window_background_opacity = 0.9,
+	-- window_background_opacity = 0.95,
+	-- font = wezterm.font("DejaVu Sans Mono"),
+	-- font = wezterm.font("Bitstream Vera Sans Mono"),
+	window_decorations = "RESIZE",
+	window_padding = {
+		top = 0,
+		right = 0,
+		bottom = 0, -- This will remove any bottom padding
+		left = 0,
+	},
 }
