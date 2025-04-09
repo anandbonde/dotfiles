054e648099c1435f4df15bcd6e3bd1b11ae12560
Anand Bonde <anand.bonde@gmail.com>
2025-04-09 11:45:22 -0400

    update configs from macos to be more practical

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,26 +1,15 @@
 local wezterm = require("wezterm")
 
-wezterm.on("update-right-status", function(window, _pane)
-	local battery_info = ""
-	for _, battery in ipairs(wezterm.battery_info()) do
-		battery_info = string.format("BATTERY: %.0f%%", battery.state_of_charge * 100)
-	end
-	window:set_right_status(wezterm.format({ { Text = battery_info } }))
-end)
-
 return {
-	-- color_scheme = "tokyonight_storm",
-	-- color_scheme = "OneDark (base16)",
-	-- color_scheme = "OneHalfDark",
-	-- color_scheme = "3024 (base16)",
-	color_scheme = "Catppuccin Frappe",
+	-- color_scheme = "Solarized Light (Gogh)",
+	-- color_scheme = "Gruvbox dark, medium (base16)",
+	color_scheme = "tokyonight-storm",
 	font_size = 16,
 	hide_tab_bar_if_only_one_tab = true,
 	line_height = 1.2,
-	-- macos_window_background_blur = 10,
-	-- window_background_opacity = 0.95,
+	-- macos_window_background_blur = 20,
+	-- window_background_opacity = 0.8,
 	-- font = wezterm.font("DejaVu Sans Mono"),
-	-- font = wezterm.font("Bitstream Vera Sans Mono"),
 	window_decorations = "RESIZE",
 	window_padding = {
 		top = 0,
