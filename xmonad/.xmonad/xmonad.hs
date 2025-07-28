452b0ef720c5578164880f7055920e2991ee129f
Anand Bonde <anand.bonde@gmail.com>
2025-07-28 16:47:23 -0400

    xmonad: config advancements

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -2,20 +2,24 @@ import XMonad
 import Control.Monad (when)
 import Data.List (isInfixOf)
 import Debug.Trace
-import XMonad.Actions.WindowGo
-import XMonad.Actions.Navigation2D
+import XMonad.Actions.CycleWS (toggleWS)
+import XMonad.Actions.WindowGo -- needed for runOrRaise
+-- import XMonad.Actions.Navigation2D
 import XMonad.Actions.GridSelect
 import XMonad.Actions.FocusNth
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops as E
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.WorkspaceHistory (workspaceHistoryHook)
+import XMonad.Layout.Accordion
 import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ToggleLayouts
-import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run (spawnPipe, safeSpawn)
 import XMonad.Util.SpawnOnce
@@ -26,12 +30,18 @@ import System.IO (hPutStrLn)
 
 myModMask = mod1Mask
 myTerminal = "st"
-myBrowser = "firefox"
+myTerminalClass = "st-256color"
+myBrowser = "thorium-browser"
+myBrowserProgramName = "thorium"
+myBrowserClass = "Thorium-browser"
+myFont = "xft:Liberation Mono:size=10"
+myGSFont = "xft:Liberation Sans:style=bold:size=14"
 
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ withNavigation2DConfig def $ withUrgencyHook MyUrgencyHook $ ewmh $ docks def
+    -- xmonad $ withNavigation2DConfig def $ withUrgencyHook MyUrgencyHook $ ewmh $ docks def
+    xmonad $ withUrgencyHook MyUrgencyHook $ ewmh $ docks def
         { modMask               = myModMask
         , terminal              = myTerminal
         , borderWidth           = 1
@@ -46,47 +56,58 @@ main = do
         , logHook               = dynamicLogWithPP xmobarPP
                                     { ppOutput = hPutStrLn xmproc
                                     , ppLayout = const "" -- hide layout name
-                                    }
+                                    } >> workspaceHistoryHook
         , startupHook           = myStartupHook
         }
-        `additionalKeys`
-        [ ((mod4Mask, xK_Return), spawn myTerminal)
-        ]
         `additionalKeysP`
+        -- [ ("M-h", windowGo L False)
+        -- , ("M-l", windowGo R False)
+        -- , ("M-j", windowGo D False)
+        -- , ("M-k", windowGo U False)
         [ ("M-f",   sendMessage ToggleStruts >> sendMessage ToggleLayout)
         , ("M-v",   namedScratchpadAction myScratchpads "term")
+        , ("M-n",   namedScratchpadAction myScratchpads "notes")
         , ("M-S-r", spawn "xmonad --recompile && xmonad --restart")
         , ("M-S-f", withFocused $ \w -> windows (W.float w (W.RationalRect 0.25 0.25 0.5 0.5)))
-        , ("M-s",   goToSelected myGSConfig)
+
+        , ("M-s",   goToSelected myGSConfigWindow)
+        , ("M-S-s", bringSelected myGSConfigWindow)
+
         , ("M-u",   focusUrgent)
-        , ("M-h",   windowGo L False)
-        , ("M-l",   windowGo R False)
-        , ("M-j",   windowGo D False)
-        , ("M-k",   windowGo U False)
-        , ("M-b",   runOrRaise "firefox" (className =? "firefox_firefox"))
-        , ("M-r",   runOrRaise "st" (className =? "st-256color"))
-        , ("M-g",   runOrRaise "steam -silent steam://rungameid/444200" (className =? "steam_app_444200"))
-        , ("M-n",   runGridActions)
+
+        , ("M-b",   runOrRaise myBrowser (className =? myBrowserClass))
+        , ("M-r",   runOrRaise myTerminal (className =? myTerminalClass))
+        , ("M-e",   runOrRaise "steam -silent steam://rungameid/444200" (className =? "steam_app_444200"))
+
+        , ("M-C-c", runGridSelect myGSConfigFunction myConfigs)
+        , ("M-C-l", runGridSelect myGSConfigFunction myLinks)
+        , ("M-C-r", runGridSelect myGSConfigFunction myPrograms)
+        , ("M-C-x", runGridSelect myGSConfigFunction mySystemActions)
+        , ("M-C-0", runGridSelect myGSConfigFunction myCopyCommands)
+
+        , ("M-`", toggleWS)
+
+        , ("<F10>", spawn "thorium-browser --new-tab chat.openai.com")
         ]
 
 
 myManageHook = composeAll
     [ className =? "steam_app_444200"   --> doShift "2:wotb"
-    , className =? "steam"              --> doShift "9:steam"
-    -- , className =? "firefox_firefox"    --> doShift "3:internet"
+    -- , className =? "steam"              --> doShift "9:steam"
     ]
 
 
-myWorkspaces :: [String]
 myWorkspaces = ["1:work", "2:wotb", "3:internet", "4", "5", "6", "7", "8", "9:steam"]
 
 
-myScratchpads = [ NS "term" spawnTerm findTerm manageTerm ]
+myScratchpads = [ NS "term"       spawnTerm       findTerm    manageTerm
+                , NS "notes"      spawnNotes      findNotes   manageTerm ]
     where
-        -- st scratchpad
-        spawnTerm   = myTerminal ++ " -n scratchpad"
-        findTerm    = resource =? "scratchpad"
-        manageTerm  = customFloating $ W.RationalRect 0.25 0.25 0.5 0.5
+        spawnTerm       = myTerminal ++ " -n ScratchTerm"
+        findTerm        = resource =? "ScratchTerm"
+        manageTerm      = customFloating $ W.RationalRect 0.25 0.25 0.5 0.5
+        spawnNotes      = myTerminal ++ " -n ScratchNotes"
+        findNotes       = resource =? "ScratchNotes"
 
 
 data MyUrgencyHook = MyUrgencyHook
@@ -98,11 +119,12 @@ instance UrgencyHook MyUrgencyHook where
             safeSpawn "notify-send" ["-u", "critical", "-t", "5000", "Steam is urgent"]
 
 
-myLayout = fullscreenFull $ avoidStruts $ toggleLayouts Full $ spacing 0 (
+myLayout = smartBorders $ fullscreenFull $ avoidStruts $ toggleLayouts Full $ spacing 8 (
             Tall        1 (3/100) (1/2)
         ||| ThreeColMid 1 (3/100) (1/2)
         ||| Mirror (Tall 1 (3/100) (1/2))
         ||| Full
+        ||| Accordion
     )
 
 
@@ -110,22 +132,73 @@ myStartupHook = do
     spawn "xset r rate 200 40"
     spawn "pgrep -x sxhkd || sxhkd &"
     spawn "nitrogen --restore"
+    spawn "pgrep steam || steam -silent steam://rungameid/444200"
+    spawn ("pgrep -x " ++ myTerminal ++ " || " ++ myTerminal)
+    spawn ("pgrep -x " ++ myBrowserProgramName ++ " || " ++ myBrowser ++ " --new-tab chatgpt.com")
 
 
-myGSConfig = def
-    { gs_cellheight = 30
+myGSConfigWindow :: GSConfig Window
+myGSConfigWindow = def
+    { gs_cellheight = 60
     , gs_cellwidth  = 200
-    , gs_font       = "xft:JetBrains Mono:size=10"
+    , gs_font       = myGSFont
     }
 
 
-myGridActions :: [(String, X ())]
-myGridActions =
-    [ ("Pull Requests",  spawn "firefox --new-tab 'https://github.com/microsoft/demikernel/pulls'")
-    , ("Github Actions", spawn "firefox --new-tab 'https://github.com/microsoft/demikernel/actions'")
-    , ("ChatGPT",        spawn "firefox --new-tab 'chatgpt.com'")
+myGSConfigFunction :: GSConfig (X ())
+myGSConfigFunction = (buildDefaultGSConfig tokyoNightStormColorizer)
+    { gs_cellheight = 60
+    , gs_cellwidth  = 200
+    , gs_font       = myGSFont
+    }
+
+
+tokyoNightStormColorizer :: a -> Bool -> X (String, String)
+tokyoNightStormColorizer _ isSelected =
+    return $
+        if isSelected
+            then ("#ffbf00", "#1f2335")  -- Selected: brighter text, slightly lighter bg
+            else ("#414868", "#cccccc")  -- Normal: muted text, base b
+
+
+myLinks =
+    [ ("ChatGPT",        spawn $ myBrowser ++ " --new-tab chatgpt.com")
+    , ("YouTube",        spawn $ myBrowser ++ " --new-tab youtube.com")
+    , ("Gmail",          spawn $ myBrowser ++ " --new-tab gmail.com")
+    , ("Pull requests",  spawn $ myBrowser ++ " --new-tab https://github.com/microsoft/demikernel/pulls")
+    , ("Github Actions", spawn $ myBrowser ++ " --new-tab https://github.com/microsoft/demikernel/actions")
     ]
 
 
-runGridActions :: X ()
-runGridActions = gridselect defaultGSConfig myGridActions >>= maybe (return ()) id
+myPrograms =
+    [ ("Terminal",  spawn myTerminal)
+    , ("Browser",   spawn myBrowser)
+    , ("Blender",   spawn "blender")
+    ]
+
+
+myConfigs =
+    [ ("Xmonad",    spawn (myTerminal ++ " -e nvim ~/.xmonad/xmonad.hs"))
+    , ("xmobar",    spawn (myTerminal ++ " -e nvim ~/.xmobarrc"))
+    , ("sxhkd",     spawn (myTerminal ++ " -e nvim ~/.config/sxhkd/sxhkdrc"))
+    , ("neovim",    spawn (myTerminal ++ " -e nvim ~/.config/nvim/init.lua"))
+    ]
+
+
+mySystemActions =
+    [ ("Restart Xmonad",    spawn "xmonad --restart")
+    , ("Shutdown",          spawn "poweroff")
+    , ("Reboot",            spawn "sudo reboot")
+    , ("Suspend",           spawn "sudo systemctl suspend")
+    ]
+
+
+myCopyCommands =
+    [ ("Xmonad errors", spawn "xmonad --recompile 2> ~/.errfile; cat ~/.errfile | xclip -selection clipboard; rm ~/.errfile")
+    , ("Xmonad config", spawn "xclip -selection clipboard ~/.xmonad/xmonad.hs")
+    ]
+
+
+runGridSelect :: GSConfig (X ()) -> [(String, X ())] -> X ()
+runGridSelect gsConfig actions =
+    gridselect gsConfig actions >>= maybe (return ()) id
